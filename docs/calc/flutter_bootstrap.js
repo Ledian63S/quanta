e5d2ba8220e6a9d8e5f6e550ce912492c5c7521c
Ledian Leka <ledian.leka@moreone.tech>
2026-03-27 21:08:17 +0100

    chore: deploy web app [skip ci]

--- docs/calc/flutter_bootstrap.js
+++ docs/calc/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3659490523" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "834884809" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
